1d89224805f7c4009fa23a5fc22ddd2d9274bfb4
m-shehata <mido_matrix6000@yahoo.com>
2017-02-05 16:59:36 +0200

    "Solve the problems"

--- project/blog/models.py
+++ project/blog/models.py
@@ -7,31 +7,29 @@ from django.db.models import permalink
 
 class Categories(models.Model):
 	cat_name = models.CharField(max_length=200)
-	cat_slug = models.SlugField(max_length=200)
+	slug = models.SlugField(max_length=200)
 	cat_date_pub = models.DateTimeField('date_pub')
-	
 	
 	def __str__(self):
 		return self.cat_name
 
-
 	@permalink
 	def get_absolute_url(self):
-		return ('view_blog_categories', None, { 'slug': self.cat_slug })
+		return ('view_blog_categories', None, { 'slug': self.slug })
 
 
 class Subscribe(models.Model):
-	cat_id = models.ForeignKey(Categories)
-	#user_id = models.ForeignKey('auth.User')
+	category= models.ForeignKey(Categories)
+	curr_user = models.ForeignKey('auth.User')
 
 class Posts(models.Model):
 	post_title =models.CharField(max_length=200)
 	slug=models.SlugField(max_length=200)
 	post_body =models.TextField()
 	image =models.ImageField()
-	category_id =models.ForeignKey(Categories)
 	publish_date =models.DateTimeField('reg_date')
 	approve =models.BooleanField()
+	category=models.ForeignKey(Categories)
 
 
 	def __str__(self):
